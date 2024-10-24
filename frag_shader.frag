68356a6d3486cc51b28cd7475386b66373f30228
Marco Bonato <marcobonato09@gmail.com>
2024-10-24 11:34:27 +0200

    Added basic textures

--- frag_shader.frag
+++ frag_shader.frag
@@ -3,7 +3,11 @@
 out vec4 frag_color;
 
 in vec4 vertex_color_out;
+in vec2 tex_coords_out;
+
+uniform sampler2D input_texture;
+uniform sampler2D input_texture_2;
 
 void main () {
-    frag_color = vertex_color_out;
+    frag_color = mix(texture(input_texture, tex_coords_out), texture(input_texture_2, tex_coords_out), 0.5)*vertex_color_out;
 };
--- main.cpp
+++ main.cpp
@@ -4,33 +4,36 @@
 #include <glm/glm.hpp>
 #include <iostream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include "shader.h"
 #include "constants.h"
 #include "player.h"
 
-GLfloat vertices[] = { // 3 vals for position, then 4 vals for color
-    -0.5f, 0.5f, 5.0f,   0.2f, 0.3f, 0.1f, 1.0f,
-    0.5f, 0.5f, 5.0f,   0.1f, 0.3f, 0.6f, 1.0f,
-    0.5f, -0.5f, 5.0f,   0.6f, 0.1f, 0.90f, 1.0f,
-    -0.5f, -0.5f, 5.0f,  0.9f, 0.3f, 0.7f, 1.0f,
+GLfloat vertices[] = { // 3 vals for position, then 4 vals for color, then 2 vals for tex coords
+    -0.5f, 0.5f, 5.0f,   1.0f, 0.0f, 0.0f, 1.0f,    0.0f, 4.0f,
+    0.5f, 0.5f, 5.0f,   0.0f, 1.0f, 0.0f, 1.0f,     4.0f, 4.0f,
+    0.5f, -0.5f, 5.0f,   0.0f, 0.0f, 1.0f, 1.0f,   4.0f, 0.0f,
+    -0.5f, -0.5f, 5.0f,  0.9f, 0.3f, 0.7f, 1.0f,   0.0f, 0.0f,
 
-    -0.5f, 0.5f, 6.0f,   0.46f, 0.2f, 0.8f, 1.0f,
-    0.5f, 0.5f, 6.0f,   0.67f, 0.69f, 0.4f, 1.0f,
-    0.5f, -0.5f, 6.0f,   0.84f, 0.20f, 0.14f, 1.0f,
-    -0.5f, -0.5f, 6.0f,  0.48f, 0.27f, 0.84f, 1.0f,
+    // -0.5f, 0.5f, 6.0f,   0.46f, 0.2f, 0.8f, 1.0f,
+    // 0.5f, 0.5f, 6.0f,   0.67f, 0.69f, 0.4f, 1.0f,
+    // 0.5f, -0.5f, 6.0f,   0.84f, 0.20f, 0.14f, 1.0f,
+    // -0.5f, -0.5f, 6.0f,  0.48f, 0.27f, 0.84f, 1.0f,
 };
 
 GLuint indices[] = { // Specifies the triangles to draw using indices from the vertices given
     2, 1, 0,
     2, 0, 3,
-    6, 4, 5,
-    6, 7, 4,
-    3, 0, 4,
-    3, 4, 7,
-    6, 5, 1,
-    6, 1, 2,
-    1, 5, 4, 
-    1, 4, 0
+    // 6, 4, 5,
+    // 6, 7, 4,
+    // 3, 0, 4,
+    // 3, 4, 7,
+    // 6, 5, 1,
+    // 6, 1, 2,
+    // 1, 5, 4, 
+    // 1, 4, 0
 };  
 
 int main() {
@@ -74,10 +77,12 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7*sizeof(GLfloat), (void*) 0);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7*sizeof(GLfloat), (void*) (3*sizeof(GLfloat)));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9*sizeof(GLfloat), (void*) 0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 9*sizeof(GLfloat), (void*) (3*sizeof(GLfloat)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 9*sizeof(GLfloat), (void*) (7*sizeof(GLfloat)));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
 
     GLuint look_at_location = glGetUniformLocation(shader.ID, "look_at");
     GLuint projection_location = glGetUniformLocation(shader.ID, "projection");
@@ -90,6 +95,38 @@ int main() {
     double prev_cursor_x, prev_cursor_y;
     glfwGetCursorPos(window, &cursor_x, &cursor_y);
     glfwGetCursorPos(window, &prev_cursor_x, &prev_cursor_y);
+
+    glActiveTexture(GL_TEXTURE0);
+    GLuint container_texture;
+    glGenTextures(1, &container_texture);
+    glBindTexture(GL_TEXTURE_2D, container_texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    int img_width, img_height, num_channels;
+    unsigned char *image_data = stbi_load("container.jpg", &img_width, &img_height, &num_channels, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_width, img_height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(image_data);
+
+    glActiveTexture(GL_TEXTURE1);
+    GLuint smiley_texture;
+    glGenTextures(1, &smiley_texture);
+    glBindTexture(GL_TEXTURE_2D, smiley_texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    stbi_set_flip_vertically_on_load(true);
+    image_data = stbi_load("awesomeface.png", &img_width, &img_height, &num_channels, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_width, img_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(image_data);
+
+    shader.execute();
+    glUniform1i(glGetUniformLocation(shader.ID, "input_texture"), 0);
+    glUniform1i(glGetUniformLocation(shader.ID, "input_texture_2"), 1);
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -125,4 +162,39 @@ int main() {
     return 0;
 }
 
-// Swap to using triangle strips
+// Keep working through the learnopengl tutorial
+// Limit upwards rotation
+// Import external models (like obj files)
+// Occlusion culling
+// Shaders
+// Lighting
+// Make sure deltaTime is working correctly, consider what happens on lag spikes
+// Implement a player model
+// Collision detection (bounding volumes?, quadtrees, BSP trees, octtrees, BVH, kd-trees), test using a floor
+// Realize that things like BSP trees can be precomputed for a reused map
+// Add inter-frame detection (required for fast moving objects)
+// Levels of detail
+// Mipmapping
+// Stencil buffer
+// Scissor test
+// Triangle strips (optimize drawing)
+// Bounding volumes for objects
+// Frustum cull objects instead of triangles?
+// Optimize triangle storage (like storing color by bitmasking uints or storing normals with only 2 values)
+// Implement frustum culling per object with bounding boxes
+// Give GPU commands all at once
+// Use instancing and batching
+// Think of the best differential equation solver to use (verlet, leapfrog, runge-kutta, satisfying contraint functions, etc.)
+// Optimize models and imported files, what if u make ur own format in binary?
+// Caching (save and reuse stuff instead of recalculating)
+// Try to make branchless code (avoid if/else statements)
+// Compute shader
+// Temporal reprojection (use info from previous frame to inform current frame)
+// Antialiasing (research the various methods)
+// Hierarchical z-buffer
+// Floating point precision is higher near 0, so check projection for outputted z values
+// Depth prepass
+// For rigid body physics (if used), see YT video by blackedout01
+// Go back through code, clean up, make more readable, optimize
+// Implement friction (how to detect if someone is sliding? (no movement accel when touching)
+// Optimize data stuff, like using short ints for indices for models
